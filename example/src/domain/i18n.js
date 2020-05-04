d08dd8f05db8981b3f2c8a2db25c7108079e6cd9
Peeke <pkuepers@kaliber.net>
2020-05-04 10:58:13 +0200

    Add extra translations for testing purposes

--- example/src/domain/i18n.js
+++ example/src/domain/i18n.js
@@ -25,11 +25,53 @@ export const i18n = {
       body: <p>Lorem ipsum dolor sit amet <strong>consectetur adipisicing elit</strong>. Facere esse unde aut officiis repudiandae vero placeat totam voluptas. Doloribus iste quae maiores officia praesentium magni, perspiciatis quibusdam necessitatibus sequi nemo.</p>,
     },
   },
+  contact: {
+    title: {
+      nl: 'Contact met ons opnemen',
+      en: 'Contact us'
+    },
+    intro: {
+      title: {
+        nl: 'Extra i18n waardes om wat realistischer de call count van normalize() te kunnen testen',
+        en: 'Extra i18n values to test the call count of normalize() more realistically'
+      },
+      body: <p>Lorem ipsum dolor sit amet <strong>consectetur adipisicing elit</strong>. Facere esse unde aut officiis repudiandae vero placeat totam voluptas. Doloribus iste quae maiores officia praesentium magni, perspiciatis quibusdam necessitatibus sequi nemo.</p>,
+      cta: {
+        label: {
+          nl: 'Neem contact op',
+          en: 'Contact us'
+        },
+        link: '#contact'
+      }
+    },
+  },
   footer: {
     visitorCount: {
       nl: n => `${n} ${n === 1 ? 'bezoeker' : 'bezoekers'} tot nu toe.`,
       en: n => `${n} ${n === 1 ? 'visitor' : 'visitors'} so far.`
     },
     copyright: () => `Copyright ${(new Date()).getFullYear()}`
+  },
+  labels: {
+    contactUs: {
+      nl: 'Neem contact op!',
+      en: 'Contact us!'
+    },
+    requestQuote: {
+      nl: 'Een offerte opvragen',
+      en: 'Request a quote',
+    },
+    back: {
+      nl: 'terug',
+      en: 'back'
+    },
+    next: {
+      nl: 'volgende',
+      en: 'next'
+    },
+    previous: {
+      nl: 'vorige',
+      en: 'previous'
+    }
   }
 }
